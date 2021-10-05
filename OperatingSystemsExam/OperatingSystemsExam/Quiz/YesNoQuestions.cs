c52bed1cd344e315e1a8b754738462c83b392931
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-10-05 13:11:40 +0200

    Version 0.4.0

--- OperatingSystemsExam/OperatingSystemsExam/Quiz/YesNoQuestions.cs
+++ OperatingSystemsExam/OperatingSystemsExam/Quiz/YesNoQuestions.cs
@@ -8,6 +8,11 @@ namespace OperatingSystemsExam.Quiz
         public static IQuestion[] Questions { get; } = new YesNoQuestion[] {
             new YesNoQuestion()
             {
+                Text = @"Die MMU übersetzt beim Paging logische Speicheradressen mit der Seitentabelle in physische Adressen.",
+                Answer = true
+            },
+            new YesNoQuestion()
+            {
                 Text = @"Journaling-Dateisysteme grenzen die bei der Konsistenzprüfung zu überprüfenden Daten ein.",
                 Answer = true
             },
@@ -18,18 +23,33 @@ namespace OperatingSystemsExam.Quiz
             },
             new YesNoQuestion()
             {
+                Text = @"Ein Journal im Dateisystem reduziert die Anzahl der Schreibzugriffe.",
+                Answer = false
+            },
+            new YesNoQuestion()
+            {
                 Text = @"Der Zugriff auf Festplatten erfolgt blockorientiert und kann wahlfrei geschehen.",
                 Answer = true
             },
             new YesNoQuestion()
             {
                 Text = @"Round Robin kann als faires Schedulingverfahren bezeichnet werden.",
-                Answer = true
+                Answer = true,
+                AlternativeQuestion = new YesNoQuestion()
+                {
+                    Text = @"Prioritätengesteuertes Scheduling kann als faires Schedulingverfahren bezeichnet werden.",
+                    Answer = false
+                }
             },
             new YesNoQuestion()
             {
                 Text = @"Mit Prioritätengesteuertem Scheduling lässt sich die geringste Gesamtlaufzeit für Aufgaben erreichen.",
-                Answer = false
+                Answer = false,
+                AlternativeQuestion = new YesNoQuestion()
+                {
+                    Text = @"Mit Stapelbetrieb lässt sich die geringste Gesamtlaufzeit für Aufgaben erreichen.",
+                    Answer = true
+                }
             },
             new YesNoQuestion()
             {
@@ -38,8 +58,23 @@ namespace OperatingSystemsExam.Quiz
             },
             new YesNoQuestion()
             {
+                Text = @"Shortest Job First garantiert die geringste durchschnittliche Laufzeit.",
+                Answer = true
+            },
+            new YesNoQuestion()
+            {
                 Text = @"Monolithische Betriebssystemkerne sind leichter zu erweitern und zu warten.",
                 Answer = false
+            },
+            new YesNoQuestion()
+            {
+                Text = @"Mikrokernel führen zu einem langsameren Betriebssystem.",
+                Answer = true
+            },
+            new YesNoQuestion()
+            {
+                Text = @"Aktives Warten vor dem Betreten eines kritischen Abschnitts verschwendet Rechenzeit.",
+                Answer = true
             },
             new YesNoQuestion()
             {
@@ -48,8 +83,18 @@ namespace OperatingSystemsExam.Quiz
             },
             new YesNoQuestion()
             {
-                Text = @"Die Funktion eines Mutex lässt sich auch mit Semaphoren erreichen.",
+                Text = @"Eine Vergrößerung des Speichers kann in manchen Ersetzungsstrategien zu mehr Seitenfehlern führen.",
                 Answer = true
+            },
+            new YesNoQuestion()
+            {
+                Text = @"Die Funktion eines Mutex lässt sich auch mit Semaphoren erreichen.",
+                Answer = true,
+                AlternativeQuestion = new YesNoQuestion()
+                {
+                    Text = @"Die Funktion einer Semaphore lässt sich auch mit einem Mutex erreichen.",
+                    Answer = false,
+                }
             }
         };
 
